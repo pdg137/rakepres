0d0903a32d2f6551bf2af46b54f81a4ddc0f3ef3
Paul Grayson <pdg@alum.mit.edu>
2011-05-02 23:37:57 -0700

    initial commit, with two examples

---
+++ test1_multiple_c_files/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "hello.h"
+
+void hello()
+{
+  printf("Hello, world!\n");
+}
---
+++ test1_multiple_c_files/main.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include "hello.h"
+
+int main()
+{
+  hello();
+  exit(0);
+}
